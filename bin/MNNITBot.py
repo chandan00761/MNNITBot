dfaeb28642072ee4eee7e6d232ec28576dc9cacf
chandanmahto <chandanmahto00761@gmail.com>
2021-01-09 16:35:02 +0530

    better bot

--- bin/MNNITBot.py
+++ bin/MNNITBot.py
@@ -9,6 +9,7 @@ class MNNITBot(discord.Client):
     def __init__(self, file, intents):
         self.default_role = None
         self.temp_role = None
+        self.admin = None
         self.admin_role = None
         self.student_file = os.path.join(os.getcwd(), file)
         with open(file) as f:
@@ -18,6 +19,7 @@ class MNNITBot(discord.Client):
     async def on_ready(self):
         self.default_role = self.guilds[0].default_role
         self.temp_role = discord.utils.get(self.guilds[0].roles, name="temp")
+        self.admin = self.guilds[0].get_member_named("Hola_Bola_Mdfkr#9935")
         self.admin_role = discord.utils.get(self.guilds[0].roles, name="Administrator")
         print(f'{self.user} has connected to Discord')
 
@@ -39,30 +41,44 @@ class MNNITBot(discord.Client):
             msgs.append(message)
         await channel.delete_messages(msgs)
 
+    async def help_message(self, message):
+        await message.reply(
+            f"""Hi {message.author.name}, welcome to MNNIT MCA Discord Server!
+To access the full server reply your registration number here.
+Here is the format: 
+!REG <year>CA<roll>
+Eg :- !REG 2020CA001""")
+
+    async def registration(self, message):
+        user_roll = re.findall("20[0-9]{2}CA[0-9]{3}", message.content)[0]
+        if user_roll in self.student_data:
+            if not self.student_data[user_roll]["visited"]:
+                self.student_data[user_roll]["visited"] = True
+                with open(self.student_file, "w") as file:
+                    json.dump(self.student_data, file)
+                member = self.guilds[0].get_member(message.author.id)
+                await member.edit(roles=[self.default_role], nick=self.student_data[user_roll]["name"])
+                await member.dm_channel.send(
+                    f"""You have successfully registered {message.author.name}. You can now access the full server."""
+                )
+            else:
+                member = self.guilds[0].get_member(message.author.id)
+                await member.dm_channel.send(
+                    f"""Looks like you have already registered on the server. If you still can't access the server 
+then contact the administrator {self.admin.mention} or tell in the WhatsApp group. """
+                )
+        else:
+            await message.reply(f"No such roll exists! {user_roll}. Please check your input.")
+
     async def on_message(self, message):
         if message.author.id == self.user.id:
             return
         if isinstance(message.channel, discord.DMChannel):
             if message.content == "!help":
-                await message.reply(f"""
-                            Hi {message.author.name}, welcome to MNNIT MCA Discord Server!
-            To access the full server reply your registration number here.
-            Here is the format: 
-                !REG <year>CA<roll>
-            Eg :- !REG 2020CA001
-                        """)
+                await self.help_message(message)
             elif message.content.startswith("!REG"):
-                user_roll = re.findall("20[0-9]{2}CA[0-9]{3}", message.content)[0]
-                if user_roll in self.student_data:
-                    if not self.student_data[user_roll]["visited"]:
-                        self.student_data[user_roll]["visited"] = True
-                        with open(self.student_file, "w") as file:
-                            json.dump(self.student_data, file)
-                        member = self.guilds[0].get_member(message.author.id)
-                        await member.edit(roles=[self.default_role], nick=self.student_data[user_roll]["name"])
+                await self.registration(message)
 
-                else:
-                    await message.reply(f"No such roll exists! {user_roll}")
         elif isinstance(message.channel, discord.TextChannel):
             if message.content == "!clear" and self.admin_role in message.author.roles:
                 await self.clear_message(message.channel)
